5365db22b28145340c13d56a9a8bac6a291c74e5
Ryan Truong <ryan.truong2304@gmail.com>
2022-03-10 01:14:23 +1100

    Got grid working with js.

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const etchContainer = document.querySelector('.etch-container');
-const etchGrid = document.createElement('div');
+const blackButton = document.querySelector('.black');
+const rainbowButton = document.querySelector('.rainbow');
+const amountButton = document.querySelector('.amount');
+const clearButton = document.querySelector('.clear');
+
+amountButton.addEventListener('click', function() {
+    let gridAmount = prompt("Please Enter A Number Between 0 - 100:");
+
+    if(gridAmount > 100 || isNaN(gridAmount) == true) {
+        prompt("Please Enter A Valid Number (0-100)");
+    } else {
+        createGrid(gridAmount);
+    }
+});
 
 //create grid based on user input
-function createGrid() {
-
+function createGrid(amount) {
+    etchContainer.style.setProperty('--grid-column', amount);
+    etchContainer.style.setProperty('--grid-rows', amount);
+    for(let i = 0; i < amount; i++) {
+        for(let j = 0; j < amount; j++) {
+            const etchGrid = document.createElement('div');
+            etchGrid.classList.add('etch-grid');
+            etchContainer.appendChild(etchGrid);
+        }
+    }
 }
