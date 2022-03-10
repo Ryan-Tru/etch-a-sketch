d5457011fff534bb954bf86dc789060e9f105e65
Ryan Truong <ryan.truong2304@gmail.com>
2022-03-11 00:11:24 +1100

    comp restarted before I pushed, not sure what happened

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rainbowButton = document.querySelector('.rainbow');
 const amountButton = document.querySelector('.amount');
 const clearButton = document.querySelector('.clear');
 let gridAmount = 16;
+let isDrawing = false;
 
 //prompts the user to enter amount of grids
 amountButton.addEventListener('click', function() {
@@ -29,5 +30,38 @@ function createGrid(amount) {
     }
 }
 
+blackButton.addEventListener("click", function() {
+    blackButton.style.backgroundColor = 'rgb(140, 140, 140)';
+    colorBlack();
+    console.log('2');
+});
 
+function mouseMove() {
+    const etchGrid = document.querySelector(".etch-container");
+    etchGrid.addEventListener('mousedown', function() {
+        isDrawing = true;
+        console.log('3');
+    });
+}
+
+function colorBlack() {
+    const etchGrid = document.querySelector(".etch-container");
+    console.log('1');
+    if(isDrawing == true) {
+        etchGrid.addEventListener("mouseout", function(event) {
+        event.target.classList.replace("etch-grid", "black-color");
+        });
+    }
+}
+
+window.addEventListener('mouseup', function() {
+    if(isDrawing === true) {
+        isDrawing = false;
+        console.log('4');
+    }
+  });
+
+
+
+mouseMove();
 createGrid(gridAmount);
